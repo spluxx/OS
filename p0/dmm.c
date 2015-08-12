a1c7e877a9b9cfa60274b7321c8c1368ae8c9d9f
Landon Cox <lpc6@duke.edu>
2015-08-12 07:38:25 -0700

    first commit

---
+++ p0/dmm.c
@@ -0,0 +1,82 @@
+#include <stdio.h>  // needed for size_t
+#include <unistd.h> // needed for sbrk
+#include <assert.h> // needed for asserts
+#include "dmm.h"
+
+/* You can improve the below metadata structure using the concepts from Bryant
+ * and OHallaron book (chapter 9).
+ */
+
+typedef struct metadata {
+  /* size_t is the return type of the sizeof operator. Since the size of an
+   * object depends on the architecture and its implementation, size_t is used
+   * to represent the maximum size of any object in the particular
+   * implementation. size contains the size of the data object or the number of
+   * free bytes
+   */
+  size_t size;
+  struct metadata* next;
+  struct metadata* prev; 
+} metadata_t;
+
+/* freelist maintains all the blocks which are not in use; freelist is kept
+ * sorted to improve coalescing efficiency 
+ */
+
+static metadata_t* freelist = NULL;
+
+void* dmalloc(size_t numbytes) {
+  /* initialize through sbrk call first time */
+  if(freelist == NULL) { 			
+    if(!dmalloc_init())
+      return NULL;
+  }
+
+  assert(numbytes > 0);
+
+  /* your code here */
+	
+  return NULL;
+}
+
+void dfree(void* ptr) {
+  /* your code here */
+}
+
+bool dmalloc_init() {
+
+  /* Two choices: 
+   * 1. Append prologue and epilogue blocks to the start and the
+   * end of the freelist 
+   *
+   * 2. Initialize freelist pointers to NULL
+   *
+   * Note: We provide the code for 2. Using 1 will help you to tackle the 
+   * corner cases succinctly.
+   */
+
+  size_t max_bytes = ALIGN(MAX_HEAP_SIZE);
+  /* returns heap_region, which is initialized to freelist */
+  freelist = (metadata_t*) sbrk(max_bytes); 
+  /* Q: Why casting is used? i.e., why (void*)-1? */
+  if (freelist == (void *)-1)
+    return false;
+  freelist->next = NULL;
+  freelist->prev = NULL;
+  freelist->size = max_bytes-METADATA_T_ALIGNED;
+  return true;
+}
+
+/* for debugging; can be turned off through -NDEBUG flag*/
+void print_freelist() {
+  metadata_t *freelist_head = freelist;
+  while(freelist_head != NULL) {
+    DEBUG("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p\t",
+	  freelist_head->size,
+	  freelist_head,
+	  freelist_head->prev,
+	  freelist_head->next);
+    freelist_head = freelist_head->next;
+  }
+  DEBUG("\n");
+}
