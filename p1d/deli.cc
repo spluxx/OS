48b9ad141d0cdf2cb12f90929f2df6d7e13b876b
Inchan Hwang <ih33@duke.edu>
2018-02-03 03:53:51 -0500

    fixed some bugs

--- p1d/deli.cc
+++ p1d/deli.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <vector>
 #include "thread.h"
 using namespace std;
@@ -23,8 +24,6 @@ struct order_info {
   }
 };
 
-vector<vector<int> > orders;
-
 int max_orders, nCashier; 
 bool full;
 
@@ -43,10 +42,8 @@ void create_maker() {
 
   while(true) {
     thread_lock(NUMBER_OF_CASHIER_LOCK);
-    printf("NUM_CASH_LOCK MAKER\n");
     breakFlag |= done;
     thread_unlock(NUMBER_OF_CASHIER_LOCK);
-    printf("NUM_CASH_UNLOCK MAKER\n");
     if(breakFlag) break;
 
     int mi = 0;
@@ -54,9 +51,7 @@ void create_maker() {
     order_info toCook;
 
     thread_lock(BOARD_LOCK);
-    printf("BOARD LOCK MAKER\n");
     while(!full) thread_wait(BOARD_LOCK, WAITING_FULL_BOARD);
-    printf("RECEIVED SIGNAL(MAKER)\n");
 
     for(int i = 0 ; i < board.size() ; i ++) {
       int diff = abs(board[i].sandwich_id - made_last);
@@ -67,108 +62,78 @@ void create_maker() {
       }
     }
 
-    printf("BOARD:\n");
-    for(int i = 0 ; i < board.size() ; i ++) {
-      printf("CASHIER:%d SANDWICH ID:%d\n", board[i].cashier_id, board[i].sandwich_id); 
-    }
-
     board.erase(board.begin()+mi); // remove from board
     full = false;
     made_last = toCook.sandwich_id; // update latest sandwich made
     order_complete[toCook.cashier_id] = 1; // cooking completed!
-    printf("SIGNAL %d %d\n", toCook.cashier_id, order_complete[toCook.cashier_id]);
     thread_signal(BOARD_LOCK, toCook.cashier_id); // signal the cashier
+    cout << "READY: cashier " << toCook.cashier_id << " sandwich " << toCook.sandwich_id << endl; 
 
-    printf("ORDER_COMPLETE:\n");
-    for(int i = 0 ; i < nCashier ; i ++) {
-      printf("ORDER FOR CASHIER %d: %d\n", i, order_complete[i]); 
-    }
 
     thread_unlock(BOARD_LOCK);
-    printf("BOARD_UNLOCK MAKER\n");
   }
 
-  printf("PEACE!\n");
+  free(order_complete);
 }
 
-void create_cashier(void* order_file) {
+void create_cashier(FILE* order_file) {
   int myID = -1;
   int current_order = 0;
 
   // -----------------USING nCashier--------
   thread_lock(NUMBER_OF_CASHIER_LOCK); //   |
-  printf("NUM_CASH_LOCK CASHIER\n");
   myID = liveCashier++;		       //   |
   thread_unlock(NUMBER_OF_CASHIER_LOCK); // |
-  printf("NUM_CASH_LOCK CASHIER %d\n", myID);
   // ---------------------------------------
   
-  while(current_order < orders[myID].size()) {
+  int sandwich = -1;
+  while(fscanf(order_file, "%d", &sandwich) != EOF) {
     thread_lock(BOARD_LOCK); 							
-    printf("BOARD_LOCK %d\n", myID);
 
     if(board.size() < max_orders) { // if there's space on the board post order 
-      printf("PUT SANDWICH %d\n", orders[myID][current_order]);
-      board.push_back(order_info(myID, orders[myID][current_order]));
+      board.push_back(order_info(myID, sandwich));
       order_complete[myID] = 0; // order is not completed			
+      cout << "POSTED: cashier " << myID << " sandwich " << sandwich << endl; 
 
       if(board.size() == max_orders) {
 	full = true;
 	thread_signal(BOARD_LOCK, WAITING_FULL_BOARD); 
-	printf("SIGNAL BOARD_FULL %d\n", myID);
       }
 
-      printf("BOARD_WAIT %d\n", myID);
       while(order_complete[myID] == 0) { // keep waiting until we get signal(order complete) from the cooker {
 	thread_wait(BOARD_LOCK, myID); // release lock, wait until signal with this ID is sent
       }
-      printf("RECEIVED_SIGNAL %d\n", myID);
       current_order ++;
     }
 
     thread_unlock(BOARD_LOCK);
-    printf("BOARD_UNLOCK %d\n", myID);
   }
 
   // -----------------USING nCashier------------------------
   thread_lock(NUMBER_OF_CASHIER_LOCK); //		  //|
-  printf("CHECK EXIT %d < %d\n", liveCashier, max_orders);
-  if(liveCashier < max_orders) {
+  if(--liveCashier == 0) done = 1;	//		    |
+  else if(liveCashier < max_orders) {
     max_orders = liveCashier;  //|
-    printf("UG...\n");
 
     thread_lock(BOARD_LOCK);
     full = true;
     thread_signal(BOARD_LOCK, WAITING_FULL_BOARD);
-    printf("SIGNAL FULL BEFORE EXIT %d\n", myID);
     thread_unlock(BOARD_LOCK);
   }
-  if(--liveCashier == 0) done = 1;	//		    |
   thread_unlock(NUMBER_OF_CASHIER_LOCK); //		    |
   // -------------------------------------------------------
 
-  printf("CASHIER %d EXIT\n", myID);
+  fclose(order_file);
 }
 
 void initialize(char* order_files[]) {
-  start_preemptions(true, true, 1);
+  //start_preemptions(true, true, 1);
   order_complete = (int *) malloc(sizeof(int)*nCashier);
 
   for(int i = 2 ; i <= nCashier+1 ; i ++) {
-    vector<int> orderForI;
-    FILE* fileForI = fopen(order_files[i], "r");
-    int sandwich_id = -1;
-    while(fscanf(fileForI, "%d", &sandwich_id) != EOF) {
-      orderForI.push_back(sandwich_id);
-    } orders.push_back(orderForI);
-    fclose(fileForI);
-  }
-
-  for(int i = 2 ; i <= nCashier+1 ; i ++) 
     thread_create((thread_startfunc_t) create_cashier, fopen(order_files[i], "r"));
+  }
   thread_create((thread_startfunc_t) create_maker, NULL);
-
-  free(order_complete);
 }
 
 int main(int argc, char* argv[]) {
@@ -177,6 +142,7 @@ int main(int argc, char* argv[]) {
   full = false;
   done = nCashier > 0 ? 0 : 1;
   sscanf(argv[1], "%d", &max_orders);
+  max_orders = max_orders < nCashier ? max_orders : nCashier;
   thread_libinit((thread_startfunc_t) initialize, argv);
   return 0;
 }
