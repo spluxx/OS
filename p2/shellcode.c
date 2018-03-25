1bcf6d998c484941db8f591bb63d76522b11def5
Gaemin Lee <gl67@duke.edu>
2018-03-25 15:26:59 -0400

    shellcode.c added

--- p2/shellcode.c
+++ p2/shellcode.c
@@ -1,4 +1,14 @@
-/* Shellcode
+/* 
+ * This is the code to span a shell in C 
+ * Followed the instructions from "Smashing the Stack for Fun and Profit" 
 */
 #include <stdio.h>
 #include <string.h>
+
+void main(){
+	char *name[2];
+	name[0]= "/bin/bash";
+	name[1] = NULL;
+	execve(name[0],name,NULL);
+	exit(0);
+}
